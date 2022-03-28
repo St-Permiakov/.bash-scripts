33bb7aa8cf3fef18482414b8223165df7a33237e
Stanislav <permyakov@aeroidea.ru>
2022-03-28 11:51:06 +0300

    new_component

---
+++ new_component.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+name=$1
+path=${PWD}/$name
+
+# Set the color variable
+green='\033[0;32m'
+# Clear the color after that
+clear='\033[0m'
+
+mkdir $path
+
+# Create index file for export
+echo "export * from './$name';" > $path/index.ts
+# Create Component file with default template
+echo "import React from 'react';
+
+import styles from './$name.scss';
+
+export const $name = () => {
+  return (
+    <div className={styles.root}>Component $name</div>
+  );
+};" > $path/$name.tsx
+# Create styles module
+echo ".root {}" > $path/$name.scss
+
+# Done!
+echo -e "${green}Done!${clear}"
