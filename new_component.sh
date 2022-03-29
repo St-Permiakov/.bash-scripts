e60075902f8b9e8abeb86a7f1adb546f6fc7bd0b
Stanislav <permyakov@aeroidea.ru>
2022-03-29 08:26:31 +0300

    now script accepts multiple arguments

--- new_component.sh
+++ new_component.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-name=$1
-path=${PWD}/$name
+for name in $@
+do
+	path=${PWD}/$name
 
-# Set the color variable
-green='\033[0;32m'
-# Clear the color after that
-clear='\033[0m'
+	# Set the color variable
+	green='\033[0;32m'
+	# Clear the color after that
+	clear='\033[0m'
 
-mkdir $path
+	mkdir $path
 
-# Create index file for export
-echo "export * from './$name';" > $path/index.ts
-# Create Component file with default template
-echo "import React from 'react';
+	# Create index file for export
+	echo "export * from './$name';" > $path/index.ts
+	# Create Component file with default template
+	echo "import React from 'react';
 
-import styles from './$name.scss';
+	import styles from './$name.scss';
 
-export const $name = () => {
-  return (
-    <div className={styles.root}>Component $name</div>
-  );
-};" > $path/$name.tsx
-# Create styles module
-echo ".root {}" > $path/$name.scss
+	export const $name = () => {
+		return (
+			<div className={styles.root}>Component $name</div>
+		);
+	};" > $path/$name.tsx
+	# Create styles module
+	echo ".root {}" > $path/$name.scss
+done
 
 # Done!
 echo -e "${green}Done!${clear}"
